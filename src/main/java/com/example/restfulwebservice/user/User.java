6eecca3ddabaac4c4a1f6f466f9e9e04d3f14852
godwook <passionate.ksw@gmail.com>
2023-01-14 23:46:02 +0900

    commit..

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -2,13 +2,19 @@ package com.example.restfulwebservice.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+// SpringBoot 2.3 버전 부터는 Web Startes에 더이상 javax.validation.*는 기본으로 포함하지 않는다.
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 public class User {
     private Integer id;
+
+    @Size(min=2)
     private String name;
+    @Past
     private Date joinDate;
 }
