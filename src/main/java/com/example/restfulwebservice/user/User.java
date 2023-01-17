f7f1a1915e2528ec79c58b69b283dc0ca25e985a
godwook <passionate.ksw@gmail.com>
2023-01-17 23:25:48 +0900

    commit about validation

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 // SpringBoot 2.3 버전 부터는 Web Startes에 더이상 javax.validation.*는 기본으로 포함하지 않는다.
 
 import javax.validation.constraints.Past;
@@ -14,6 +15,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 //@JsonIgnoreProperties(value={"password"})
+@NoArgsConstructor
 @JsonFilter("UserInfo")
 public class User {
     private Integer id;
