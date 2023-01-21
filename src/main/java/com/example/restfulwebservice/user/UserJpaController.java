b273a734d3768772a5859694cf9ade4049c66503
godwook <passionate.ksw@gmail.com>
2023-01-21 09:52:01 +0900

    commit about JPA

--- src/main/java/com/example/restfulwebservice/user/UserJpaController.java
+++ src/main/java/com/example/restfulwebservice/user/UserJpaController.java
@@ -1,6 +1,5 @@
 package com.example.restfulwebservice.user;
 
-import jdk.internal.loader.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
